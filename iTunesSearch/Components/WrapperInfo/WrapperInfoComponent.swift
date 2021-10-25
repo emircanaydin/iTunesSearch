3301feeab9a2e2d7faa2a1a7cdb0f0391976b2ea
emircanaydin <emircanaydin@menulux.com>
2021-10-26 01:07:14 +0300

    WrapperInfoComponent is created.

---
+++ iTunesSearch/Components/WrapperInfo/WrapperInfoComponent.swift
@@ -0,0 +1,84 @@
+//
+//  WrapperInfoComponent.swift
+//  iTunesSearch
+//
+//  Created by Emircan Aydın on 26.10.2021.
+//
+
+import UIKit
+
+class WrapperInfoComponent: GenericBaseView<WrapperInfoComponentData> {
+    
+    private lazy var stackView: UIStackView = {
+        let temp = UIStackView(arrangedSubviews: [artistName, trackName, collectionName])
+        temp.translatesAutoresizingMaskIntoConstraints = false
+        temp.alignment = .center
+        temp.distribution = .fill
+        temp.axis = .vertical
+        temp.setCustomSpacing(5, after: artistName)
+        temp.setCustomSpacing(3, after: trackName)
+        return temp
+    }
+    
+    private lazy var artistName: UILabel = {
+        let temp = UILabel()
+        temp.translatesAutoresizingMaskIntoConstraints = false
+        temp.textColor = ColorHelper.gray.value
+        temp.text = " "
+        temp.lineBreakMode = .byTruncatingTail
+        temp.numberOfLines = 1
+        temp.contentMode = .left
+        temp.textAlignment = .left
+        temp.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        return temp
+    }()
+    
+    private lazy var trackName: UILabel = {
+        let temp = UILabel()
+        temp.translatesAutoresizingMaskIntoConstraints = false
+        temp.textColor = ColorHelper.darkGray.value
+        temp.text = " "
+        temp.lineBreakMode = .byTruncatingTail
+        temp.numberOfLines = 1
+        temp.contentMode = .left
+        temp.textAlignment = .left
+        temp.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        return temp
+    }()
+    
+    private lazy var collectionName: UILabel = {
+        let temp = UILabel()
+        temp.translatesAutoresizingMaskIntoConstraints = false
+        temp.textColor = ColorHelper.gray.value
+        temp.text = " "
+        temp.lineBreakMode = .byTruncatingTail
+        temp.numberOfLines = 1
+        temp.contentMode = .left
+        temp.textAlignment = .left
+        temp.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        return temp
+    }()
+    
+    override func addMajorViewComponents() {
+        super.addMajorViewComponents()
+        addComponents()
+    }
+    
+    override func loadDataView() {
+        guard let data = returnData() else { return }
+        artistName.text = data.artistName
+        trackName.text = data.trackName
+        collectionName.text = data.collectionName
+    }
+    
+    private func addComponents() {
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+    }
+}
--- iTunesSearch/Utilities/ColorHelper/ColorHelper.swift
+++ iTunesSearch/Utilities/ColorHelper/ColorHelper.swift
@@ -16,8 +16,14 @@ enum ColorHelper: GenericValueProtocol {
         switch self {
         case .priceContainerBackgorund:
             return #colorLiteral(red: 0.7254901961, green: 0.6784313725, blue: 0.5098039216, alpha: 1)
+        case .gray:
+            return #colorLiteral(red: 0.4392156863, green: 0.4392156863, blue: 0.4392156863, alpha: 1)
+        case .darkGray:
+            return #colorLiteral(red: 0.2823529412, green: 0.2823529412, blue: 0.2823529412, alpha: 1)
         }
     }
     
     case priceContainerBackgorund
+    case gray
+    case darkGray
 }
