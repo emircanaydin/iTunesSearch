6798ea35c3455f5e53d8ff44e19315a9c12cf8cf
emircanaydin <emircanaydin@menulux.com>
2021-10-27 02:18:44 +0300

    ItemCollection added in SearchViewController.

--- iTunesSearch/SceneModules/Search/SearchViewController.swift
+++ iTunesSearch/SceneModules/Search/SearchViewController.swift
@@ -9,16 +9,34 @@ import UIKit
 
 class SearchViewController: BaseViewController<SearchViewModel> {
     
+    private var searchResultCollection: ItemCollectionView!
     private var searchControllerComponent: SearchControllerComponent!
     
     override func prepareViewControllerConfigurations() {
         view.backgroundColor = .white
         addSearchComponent()
+        addSearchResultCollection()
     }
     
     // MARK: - Private Methods
     private func addSearchComponent() {
         searchControllerComponent = SearchControllerComponent(viewModel: viewModel.getSearchControllerComponentData())
         navigationItem.searchController = searchControllerComponent
+    }
+    
+    private func addSearchResultCollection() {
+        searchResultCollection = ItemCollectionView()
+        searchResultCollection.translatesAutoresizingMaskIntoConstraints = false
+        searchResultCollection.delegate = viewModel
+        
+        view.addSubview(searchResultCollection)
+        
+        NSLayoutConstraint.activate([
+            
+            searchResultCollection.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchResultCollection.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchResultCollection.topAnchor.constraint(equalTo: view.topAnchor),
+            searchResultCollection.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
     }
 }
