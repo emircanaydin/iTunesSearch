3e2a6c27f00b138da7b69863b2ffd0e4a9f8049f
emircanaydin <emircanaydin@menulux.com>
2021-10-28 17:26:53 +0300

    LottieViewController, LottieManager and LottieHelper's is created.

---
+++ iTunesSearch/Managers/LottieManager/LottieManagerProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  LottieManagerProtocol.swift
+//  iTunesSearch
+//
+//  Created by Emircan Aydın on 28.10.2021.
+//
+
+import Foundation
+
+protocol LottieManagerProtocol {
+
+    func onPreExecute()
+    
+    func onPostExecute()
+    
+}
